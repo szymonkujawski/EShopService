29dc7290306b6d65658cd5ecac1498b1a4145513
s-A301-02 <s-A301-02@K-A301-02>
2025-03-03 18:15:43 +0100

    Dodaj pliki projektów.

---
+++ EShopService/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace EShopService.Models
+{
+    public class Category
+    {
+        //public string name { get; set; };
+        public string name { get; set; } = default!;
+    }
+}
---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace EShopService.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string name { get; set; } = default!;
+
+        public Category category { get; set; } = default!;
+
+        public Guid created_by { get; set; }
+
+    }
+}
