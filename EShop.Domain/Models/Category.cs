654e407a6281936ffbfe45d2f525d9e52c03021e
s-A301-02 <s-A301-02@K-A301-02>
2025-03-24 18:08:56 +0100

    24.03.2025

---
+++ EShop.Domain/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace EShopService.Models;
+
+public class Category : BaseModel
+{
+    public string Name { get; set; }
+}
---
+++ EShop.Domain/Repositories/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using EShopService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Repositories
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions < DataContext > options) : base(options) {}
+
+    public DbSet<Product> Products { get; set; }
+    }
+}
